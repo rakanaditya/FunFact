5c5b82192b2ce32eb38054f5e5ce47fd6959484f
Rakan Aditya <56795337+rakanaditya@users.noreply.github.com>
2026-06-27 00:34:13 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
  Version 4.0.0
 ======================================================*/
 
-const CACHE_NAME = "funfact-v4-cache-v4.1";
+const CACHE_NAME = "funfact-v4-cache-v4.2";
 
 const STATIC_CACHE = [
 
