49e805be27bb954fe207c3b12981daee08beadfa
Rakan Aditya <56795337+rakanaditya@users.noreply.github.com>
2026-06-28 02:02:01 +0700

    Update cache name to version 4.4

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
  Version 4.0.0
 ======================================================*/
 
-const CACHE_NAME = "funfact-v4-cache-v4.3";
+const CACHE_NAME = "funfact-v4-cache-v4.4";
 
 const STATIC_CACHE = [
 /*aktif ini profesional*/
